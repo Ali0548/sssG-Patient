8b7c657f01afd6939bb75e32ab4bbe7f01b7ef8d
Ali0548 <abhai0548@gmail.com>
2023-10-15 23:07:33 +0500

    Patient Service First Commit

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'boxicons/css/boxicons.min.css';
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
